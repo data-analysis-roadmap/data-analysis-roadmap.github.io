40ffb7f94b71c4bf036d65ee6da2374368d46014
sebolden <56839633+sebolden@users.noreply.github.com>
2022-10-13 14:18:51 -0400

    -

---
+++ assets/js/13_aos_V6.js
@@ -0,0 +1,83 @@
+
+
+function home() {
+    var view0 = document.getElementById("view0");
+    var view1 = document.getElementById("view1");
+    var view2 = document.getElementById("view2");
+    var view3 = document.getElementById("view3");
+    var view4 = document.getElementById("view4");
+      view0.style.display = "block";
+      view1.style.display = "none";
+      view2.style.display = "none";
+      view3.style.display = "none";
+      view4.style.display = "none";
+  }
+
+
+function view1() {
+    var view0 = document.getElementById("view0");
+    var view1 = document.getElementById("view1");
+    var view2 = document.getElementById("view2");
+    var view3 = document.getElementById("view3");
+    var view4 = document.getElementById("view4");
+      view1.style.display = "block";
+      view0.style.display = "none";
+      view2.style.display = "none";
+      view3.style.display = "none";
+      view4.style.display = "none";
+  }
+  
+
+function view2() {
+    var view0 = document.getElementById("view0");
+    var view1 = document.getElementById("view1");
+    var view2 = document.getElementById("view2");
+    var view3 = document.getElementById("view3");
+    var view4 = document.getElementById("view4");
+      view2.style.display = "block";
+      view0.style.display = "none";
+      view1.style.display = "none";
+      view3.style.display = "none";
+      view4.style.display = "none";
+  }
+  
+  function view2() {
+    var view0 = document.getElementById("view0");
+    var view1 = document.getElementById("view1");
+    var view2 = document.getElementById("view2");
+    var view3 = document.getElementById("view3");
+    var view4 = document.getElementById("view4");
+      view2.style.display = "block";
+      view0.style.display = "none";
+      view1.style.display = "none";
+      view3.style.display = "none";
+      view4.style.display = "none";
+  }
+  
+  function view3() {
+    var view0 = document.getElementById("view0");
+    var view1 = document.getElementById("view1");
+    var view2 = document.getElementById("view2");
+    var view3 = document.getElementById("view3");
+    var view4 = document.getElementById("view4");
+      view3.style.display = "block";
+      view0.style.display = "none";
+      view1.style.display = "none";
+      view2.style.display = "none";
+      view4.style.display = "none";
+  }
+  
+  
+  function view4() {
+    var view0 = document.getElementById("view0");
+    var view1 = document.getElementById("view1");
+    var view2 = document.getElementById("view2");
+    var view3 = document.getElementById("view3");
+    var view4 = document.getElementById("view4");
+      view4.style.display = "block";
+      view0.style.display = "none";
+      view1.style.display = "none";
+      view2.style.display = "none";
+      view3.style.display = "none";
+  }
+  
